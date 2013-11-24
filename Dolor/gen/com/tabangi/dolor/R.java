1883a4490275a00a6ea20961162a27d230c0b52c
Richard Tamala Jr <onyotzki@gmail.com>
2013-11-24 13:19:00 +0800

    General Update 05
    1) Bug fixes
    2) UI updates

--- Dolor/gen/com/tabangi/dolor/R.java
+++ Dolor/gen/com/tabangi/dolor/R.java
@@ -27,10 +27,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int bg_login=0x7f020000;
-        public static final int btn_facebook=0x7f020001;
-        public static final int btn_singup=0x7f020002;
-        public static final int ic_action_search=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
+        public static final int bg_plain=0x7f020001;
+        public static final int btn_facebook=0x7f020002;
+        public static final int btn_singup=0x7f020003;
+        public static final int ic_action_search=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
     }
     public static final class id {
         public static final int TextView02=0x7f090027;
